ef6300e6e70358b1958a477a706ae8e3a1e5be43
Vedantsharma90909 <82584412+Vedantsharma90909@users.noreply.github.com>
2021-06-25 15:03:33 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,21 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",mouseclick);
+function mouseclick(e){
+    mousex=e.clientX-canvas.offsetLeft;
+    mousey=e.clientY-canvas.offsetTop;
+    circle(mousex,mousey);
+}
+function circle(mousex,mousey){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mousex,mousey,40,0,2*Math.PI);
+    ctx.stroke();
+}
